bd93c530789a5c0066812f938f0a06873c8cf331
Daniel Sharon <daanielsharon@gmail.com>
2022-07-09 09:58:13 +0700

    Removing console

--- middleware/verifyuserrole.js
+++ middleware/verifyuserrole.js
@@ -13,27 +13,3 @@ exports.verifyRoles = async (req, res, next) => {
 		console.log(error.message);
 	}
 };
-
-// exports.verifyRoles = async () => {
-// 	return (req, res, next) => {
-// 		console.log(req.role);
-// 		if (!req.role) return res.sendStatus(401);
-// 		if (req.role == 'admin') return res.status(200);
-// 		next();
-// 	};
-// };
-
-// exports.AdminOrNot = async (req, res, next) => {
-// 	try {
-// 		const user = await tbl_anggota.findOne({
-// 			where: {
-// 				email: req.body.email,
-// 			},
-// 		});
-// 		if (user == null) return res.status(404).send('User Not Found');
-// 		if (user[0].role !== 'admin') return res.status(401).send('Not Allowed');
-// 		next();
-// 	} catch (error) {
-// 		console.log(error.message);
-// 	}
-// };
