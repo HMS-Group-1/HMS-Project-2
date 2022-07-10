ea7da0e85971ded6581df662bb36af60a8eb3b6a
Daniel Sharon <daanielsharon@gmail.com>
2022-07-10 11:11:56 +0700

    Restricting user only pages

--- middleware/verifyuserrole.js
+++ middleware/verifyuserrole.js
@@ -3,10 +3,20 @@ const tbl_anggota = models.tbl_anggota;
 
 // Admin verification
 
-exports.verifyRoles = async (req, res, next) => {
+exports.verifyAdmin = async (req, res, next) => {
 	try {
 		if (!req.role) return res.sendStatus(401);
 		if (req.role !== 'admin') return res.sendStatus(401);
+		next();
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+exports.verifyUser = async (req, res, next) => {
+	try {
+		if (!req.role) return res.sendStatus(401);
+		if (req.role !== 'anggota') return res.sendStatus(401);
 		next();
 	} catch (error) {
 		console.log(error.message);
--- route/routes.js
+++ route/routes.js
@@ -4,7 +4,7 @@ const { getBooks, createBooks, updateBooks, deleteBooks, getBookById } = require
 const { refreshToken } = require('../controller/getrefreshtoken');
 const { Register, Login, Logout } = require('../controller/useraccesscontrol');
 const { loginValidation, registerValidation } = require('../middleware/validator');
-const { verifyRoles } = require('../middleware/verifyuserrole');
+const { verifyAdmin, verifyUser } = require('../middleware/verifyuserrole');
 const { verifyUserToken } = require('../middleware/verifyusertoken');
 
 const router = express.Router();
@@ -16,19 +16,19 @@ router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 
 // router book
-router.get('/book', verifyUserToken, getBooks);
-router.get('/book/:id', verifyUserToken, getBookById);
+router.get('/book', verifyUserToken, verifyUser, getBooks);
+router.get('/book/:id', verifyUserToken, verifyUser, getBookById);
 
 // router admin--user
-router.get('/admin/user', verifyUserToken, verifyRoles, getAllUsers);
-router.patch('/admin/updateUser/:id', verifyUserToken, verifyRoles, updateUsers);
-router.delete('/admin/deleteUser/:id', verifyUserToken, verifyRoles, deleteUsers);
+router.get('/admin/user', verifyUserToken, verifyAdmin, getAllUsers);
+router.patch('/admin/updateUser/:id', verifyUserToken, verifyAdmin, updateUsers);
+router.delete('/admin/deleteUser/:id', verifyUserToken, verifyAdmin, deleteUsers);
 
 // router admin--book
-router.get('/admin/book', verifyUserToken, verifyRoles, getBooks);
-router.get('/admin/book/:id', verifyUserToken, verifyRoles, getBookById);
-router.post('/admin/createBook', verifyUserToken, verifyRoles, createBooks);
-router.patch('/admin/updateBook/:id', verifyUserToken, verifyRoles, updateBooks);
-router.delete('/admin/deleteBook/:id', verifyUserToken, verifyRoles, deleteBooks);
+router.get('/admin/book', verifyUserToken, verifyAdmin, getBooks);
+router.get('/admin/book/:id', verifyUserToken, verifyAdmin, getBookById);
+router.post('/admin/createBook', verifyUserToken, verifyAdmin, createBooks);
+router.patch('/admin/updateBook/:id', verifyUserToken, verifyAdmin, updateBooks);
+router.delete('/admin/deleteBook/:id', verifyUserToken, verifyAdmin, deleteBooks);
 
 module.exports = router;
