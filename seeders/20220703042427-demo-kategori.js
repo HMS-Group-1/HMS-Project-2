adb45b4816552defc3bb3be88a3576c3da5f9922
Daniel Sharon <daanielsharon@gmail.com>
2022-07-20 23:46:54 +0700

    Fixing Error & Adding More Seeder and Generator

--- seeders/20220703042427-demo-kategori.js
+++ seeders/20220703042427-demo-kategori.js
@@ -1,24 +1,17 @@
 'use strict';
 
+const categoryGenerator = (kategori_nama) => {
+	let obj = {
+		kategori_nama: kategori_nama,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	};
+	return obj;
+};
+
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		return queryInterface.bulkInsert('tbl_kategoris', [
-			{
-				kategori_nama: 'finance',
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				kategori_nama: 'engineering',
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-			{
-				kategori_nama: 'politics',
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			},
-		]);
+		return queryInterface.bulkInsert('tbl_kategoris', [categoryGenerator('finance'), categoryGenerator('engineering'), categoryGenerator('politics')]);
 	},
 
 	async down(queryInterface, Sequelize) {
