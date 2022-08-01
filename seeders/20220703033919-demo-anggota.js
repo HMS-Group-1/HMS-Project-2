6452dbf3823ef6b0da7fef9dd3ed560d6faaa9d2
Daniel Sharon <daanielsharon@gmail.com>
2022-08-01 20:24:50 +0700

    Adding faker.js

--- seeders/20220703033919-demo-anggota.js
+++ seeders/20220703033919-demo-anggota.js
@@ -1,45 +1,19 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
-
-const randomNumbers = (max) => {
-	return Math.floor(Math.random() * max);
-};
-
-const randomNumbersMinMax = (min = 8, max = 15) => {
-	const difference = max - min;
-	let result = randomNumbers(difference);
-	result = result + min;
-	return result;
-};
-
-const randomNames = (length) => {
-	let result = '';
-	const characters = 'abcdefghijklmnopqrstuvwxyz';
-	const charactersLength = characters.length;
-	for (let i = 0; i < length; i++) {
-		result += characters.charAt(randomNumbers(charactersLength));
-	}
-	return result;
-};
-
-const randomRoles = () => {
-	const roles = ['admin', 'anggota'];
-	const randomRoles = roles[randomNumbers(roles.length)];
-	return randomRoles;
-};
+const { faker } = require('@faker-js/faker');
 
 const passwordGenerator = (password) => {
 	return bcrypt.hashSync(password, 10);
 };
 
-const anggotaGenerator = (email, password) => {
+const anggotaGenerator = (password) => {
 	let obj = {
-		nama: randomNames(randomNumbersMinMax()),
-		email: email,
+		nama: faker.name.findName(),
+		email: faker.internet.email(),
 		password: passwordGenerator(password),
-		no_telp: randomNumbers(1000000000),
-		role: randomRoles(),
+		no_telp: faker.phone.number('#########'),
+		role: faker.helpers.arrayElement(['admin', 'anggota']),
 		createdAt: new Date(),
 		updatedAt: new Date(),
 	};
@@ -49,35 +23,32 @@ const anggotaGenerator = (email, password) => {
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		return queryInterface.bulkInsert('tbl_anggota', [
-			anggotaGenerator('john@john.com', 'johnny'),
-			anggotaGenerator('Deee@a.com', 'Dea123'),
-			anggotaGenerator('Tony@star.com', 'Touny'),
-			anggotaGenerator('a@a.com', 'a'.repeat(4)),
-			anggotaGenerator('b@b.com', 'b'.repeat(4)),
-			anggotaGenerator('c@c.com', 'c'.repeat(4)),
-			anggotaGenerator('d@d.com', 'd'.repeat(4)),
-			anggotaGenerator('e@e.com', 'e'.repeat(4)),
-			anggotaGenerator('f@f.com', 'f'.repeat(4)),
-			anggotaGenerator('g@g.com', 'g'.repeat(4)),
-			anggotaGenerator('h@h.com', 'h'.repeat(4)),
-			anggotaGenerator('i@i.com', 'i'.repeat(4)),
-			anggotaGenerator('j@j.com', 'j'.repeat(4)),
-			anggotaGenerator('k@k.com', 'k'.repeat(4)),
-			anggotaGenerator('l@l.com', 'l'.repeat(4)),
-			anggotaGenerator('m@m.com', 'm'.repeat(4)),
-			anggotaGenerator('n@n.com', 'n'.repeat(4)),
-			anggotaGenerator('o@o.com', 'o'.repeat(4)),
-			anggotaGenerator('p@p.com', 'p'.repeat(4)),
-			anggotaGenerator('q@q.com', 'q'.repeat(4)),
-			anggotaGenerator('r@r.com', 'r'.repeat(4)),
-			anggotaGenerator('s@s.com', 's'.repeat(4)),
-			anggotaGenerator('t@t.com', 't'.repeat(4)),
-			anggotaGenerator('u@u.com', 'u'.repeat(4)),
-			anggotaGenerator('v@v.com', 'v'.repeat(4)),
-			anggotaGenerator('w@w.com', 'w'.repeat(4)),
-			anggotaGenerator('x@x.com', 'x'.repeat(4)),
-			anggotaGenerator('y@y.com', 'y'.repeat(4)),
-			anggotaGenerator('z@z.com', 'z'.repeat(4)),
+			anggotaGenerator('a'.repeat(4)),
+			anggotaGenerator('b'.repeat(4)),
+			anggotaGenerator('c'.repeat(4)),
+			anggotaGenerator('d'.repeat(4)),
+			anggotaGenerator('e'.repeat(4)),
+			anggotaGenerator('f'.repeat(4)),
+			anggotaGenerator('g'.repeat(4)),
+			anggotaGenerator('h'.repeat(4)),
+			anggotaGenerator('i'.repeat(4)),
+			anggotaGenerator('j'.repeat(4)),
+			anggotaGenerator('k'.repeat(4)),
+			anggotaGenerator('l'.repeat(4)),
+			anggotaGenerator('m'.repeat(4)),
+			anggotaGenerator('n'.repeat(4)),
+			anggotaGenerator('o'.repeat(4)),
+			anggotaGenerator('p'.repeat(4)),
+			anggotaGenerator('q'.repeat(4)),
+			anggotaGenerator('r'.repeat(4)),
+			anggotaGenerator('s'.repeat(4)),
+			anggotaGenerator('t'.repeat(4)),
+			anggotaGenerator('u'.repeat(4)),
+			anggotaGenerator('v'.repeat(4)),
+			anggotaGenerator('w'.repeat(4)),
+			anggotaGenerator('x'.repeat(4)),
+			anggotaGenerator('y'.repeat(4)),
+			anggotaGenerator('z'.repeat(4)),
 		]);
 	},
 
--- seeders/20220703042708-demo-buku.js
+++ seeders/20220703042708-demo-buku.js
@@ -1,5 +1,6 @@
 'use strict';
 const fs = require('fs');
+const { faker } = require('@faker-js/faker');
 
 const randomNumbers = (max) => {
 	return Math.floor(Math.random() * max);
@@ -23,6 +24,15 @@ const tahunTerbit = () => {
 	return tahun.join('');
 };
 
+const deskripsi = () => {
+	let paragraf = '';
+	for (let i = 0; i <= 50; i++) {
+		const result = faker.lorem.sentences(6);
+		paragraf += result;
+	}
+	return paragraf;
+};
+
 // perlu diubah sesuai dengan jumlah tbl_rak
 const rak_idGenerator = () => {
 	return randomNumbersMinMax(1, 27);
@@ -41,8 +51,7 @@ const bookGenerator = (judul_buku, path) => {
 	let obj = {
 		judul_buku: judul_buku,
 		kategori_id: categoryGenerator(),
-		deskripsi:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime quaerat repellat expedita. Sapiente, tenetur cupiditate, dolor autem fugiat porro debitis possimus sint veritatis asperiores vitae, amet numquam illum modi necessitatibus repudiandae! Animi ea beatae consequuntur nisi perferendis deserunt necessitatibus! Quaerat, earum neque. Rerum reiciendis magnam possimus beatae labore autem in ipsum facilis nesciunt doloribus laudantium sequi aliquam saepe incidunt natus ad deleniti illo voluptates molestiae, velit esse consequuntur quia expedita inventore. Asperiores ex odio nostrum veritatis nam cumque enim tenetur sed est natus repellendus, labore rem! Ipsum minima provident culpa dolor autem atque cumque pariatur fuga laudantium adipisci aperiam hic, illo repellat voluptas harum, quae tempora tenetur velit! Saepe ea est laboriosam neque, velit molestias voluptatibus. Pariatur voluptate sapiente cupiditate molestiae ea. Eveniet ad optio voluptates omnis qui maxime. Ex facilis repellat ipsa velit, autem quas. Corporis porro reiciendis sint hic aliquid quia dignissimos dolorem in harum quaerat temporibus corrupti assumenda nam culpa ducimus labore inventore, fuga unde eveniet quas itaque, est, nostrum atque. Tempora, quia! Culpa labore sequi aliquam autem rerum expedita atque? Laborum vero eveniet optio mollitia rerum esse placeat, quo fugit corrupti quasi voluptate accusamus dolorum adipisci. Provident id accusantium quam suscipit, perspiciatis reprehenderit a consequuntur beatae non iure. Aspernatur, expedita. Dolore beatae et harum omnis a odio voluptatibus mollitia. Deserunt, ducimus! Aliquid ipsa facere modi repudiandae deserunt placeat quasi id nisi laudantium dolorem dignissimos sapiente delectus minus culpa, voluptatum quis, quam aut at, sed sequi est. Hic excepturi tenetur illo similique quo explicabo, maxime dolores rem quae laboriosam error tempore doloribus repudiandae vitae aperiam eum, est sequi sunt iure. Beatae, officiis voluptas! Ex nisi architecto quaerat assumenda pariatur consequatur nesciunt expedita dolores iste hic iure aut nulla at, eius perspiciatis! Animi repellendus eos recusandae, fugit, dignissimos culpa commodi vero maxime ex autem error, exercitationem accusamus quidem aspernatur pariatur esse quos amet? Reprehenderit blanditiis qui ipsam quidem corporis laudantium illum asperiores tempora pariatur, deleniti voluptates, laboriosam cum porro ut perferendis autem. Eos reiciendis voluptatum eaque iure assumenda repellat vitae culpa, quaerat accusamus omnis atque. Quisquam enim in delectus provident cum doloribus quibusdam minima libero suscipit odio? Dolorum nesciunt nostrum minima atque, itaque eaque ut. Aut sit neque accusantium! Maiores dolor et numquam, animi esse ea deleniti laboriosam architecto nobis amet, voluptate voluptates, quod eveniet eligendi iste officiis hic dolorem repellat nisi nemo labore nam harum? Est voluptatibus nemo dolore tempora ab accusantium necessitatibus modi laborum ipsum non delectus mollitia ipsam corporis id impedit corrupti sed sequi eveniet reiciendis tempore expedita omnis, hic deleniti placeat. Velit, sapiente optio pariatur illum facere ratione a nesciunt sit eos minus iure ab harum voluptas labore quae, unde consequatur maiores perspiciatis neque voluptates quasi ad doloremque similique quibusdam? Ipsa eveniet blanditiis nesciunt odit sed beatae commodi accusamus doloribus quis sequi, qui quo laboriosam id facilis in, temporibus natus veniam perspiciatis corrupti nostrum quod iure voluptas? Ut aperiam asperiores praesentium dolor commodi, facere labore sunt ipsam, enim obcaecati id voluptates maxime blanditiis vitae deleniti, distinctio quaerat animi recusandae repudiandae quos. Ipsum, ullam.',
+		deskripsi: deskripsi(),
 		gambar: fs.readFileSync(`./public/images/${path}.jpg`),
 		stok: stockGenerator(),
 		rak_id: rak_idGenerator(),
