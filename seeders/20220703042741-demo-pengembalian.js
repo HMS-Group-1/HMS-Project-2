b51dc4c92c791ff6bfe443a8a7535ddad36e2d9d
Daniel Sharon <daanielsharon@gmail.com>
2022-08-04 15:31:47 +0700

    Adding seeder for pengembalian & peminjaman

--- seeders/20220703042741-demo-pengembalian.js
+++ seeders/20220703042741-demo-pengembalian.js
@@ -1,24 +1,37 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+const pengembalianGenerator = () => {
+	let array = [];
+	let strictOrder = [];
+	let increment = 1;
+	let id = 1;
+	let incrementZero = 0;
+	for (let i = 0; i < 14; i++) {
+		strictOrder.push(increment);
+		increment++;
+	}
+	for (let i = 0; i < 14; i++) {
+		let obj = {
+			id: id,
+			anggota_id: strictOrder[incrementZero],
+			buku_id: id,
+			isKembali: false,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+		id++;
+		incrementZero++;
+		array.push(obj);
+	}
+	return array;
+};
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		return queryInterface.bulkInsert('tbl_pengembalians', pengembalianGenerator());
+	},
+
+	async down(queryInterface, Sequelize) {
+		return queryInterface.bulkDelete('tbl_pengembalians', null, {});
+	},
 };
