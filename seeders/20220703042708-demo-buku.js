de0794034214ae4500a28f730585bec065bb607f
Daniel Sharon <daanielsharon@gmail.com>
2022-08-12 23:12:47 +0700

    trying to fix

--- seeders/20220703042708-demo-buku.js
+++ seeders/20220703042708-demo-buku.js
@@ -35,7 +35,7 @@ const stockGenerator = () => {
 
 // perlu diubah sesuai dengan jumlah tbl_kategori
 const categoryGenerator = () => {
-	return randomNumbersMinMax(1, 31);
+	return randomNumbersMinMax(1, 30);
 };
 
 const bookGenerator = (judul_buku, path) => {
