1a6fb1e0d6eb9a1afec6277287d8ab91b5be5dac
Daniel Sharon <daanielsharon@gmail.com>
2022-08-13 09:23:34 +0700

    Finishing

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
 		dialect: 'mysql',
 	},
 	production: {
-		username: 'b9cef5082c7e35',
-		password: '10638988',
-		database: 'heroku_c5a85ac24f1d401',
-		host: 'eu-cdbr-west-03.cleardb.net',
-		dialect: 'mysql',
+		username: process.env.USERNAME,
+		password: process.env.PASSWORD,
+		database: process.env.DATABASE,
+		host: process.env.HOST,
+		dialect: process.env.DIALECT,
 	},
 };
